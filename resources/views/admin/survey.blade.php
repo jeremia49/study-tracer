137cb5a45399d6bb6c0909f38d4fee291e787f77
jeremia49 <39850621+jeremia49@users.noreply.github.com>
2023-05-31 01:06:59 +0700

    survey page

--- resources/views/admin/survey.blade.php
+++ resources/views/admin/survey.blade.php
@@ -121,24 +121,61 @@
                     </div>
                     <!-- Modal body -->
                     <div class="p-6 space-y-6">
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            With less than a month to go before the European Union enacts new consumer privacy laws for its
-                            citizens, companies around the world are updating their terms of service agreements to comply.
-                        </p>
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25
-                            and is meant to ensure a common set of data rights in the European Union. It requires
-                            organizations to notify users as soon as possible of high-risk data breaches that could
-                            personally affect them.
-                        </p>
-                    </div>
-                    <!-- Modal footer -->
-                    <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="newSurveyModal" type="button"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                            accept</button>
-                        <button data-modal-hide="newSurveyModal" type="button"
-                            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                        <form action="{{ route('login') }}" method="POST" class="w-full" autocomplete="off">
+
+                            @csrf
+
+                            <div class="mt-2">
+                                <label for="nama"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Survey</label>
+                                <input type="text" id="nama" name="nama" class="style-input bg-gray-50"
+                                    placeholder="" required />
+                                @error('nama')
+                                    <span class="text-pink-500 text-sm">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="mt-2">
+                                <label for="periode"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tahun
+                                    Survey</label>
+                                <input type="number" id="periode" name="periode" class="style-input bg-gray-50"
+                                    placeholder="" required />
+                                @error('periode')
+                                    <span class="text-pink-500 text-sm">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="mt-2">
+                                <label for="status"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status
+                                    Survey</label>
+
+                                <select id="status"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option value="-1">Pilih Status</option>
+                                    <option value="1">Aktif</option>
+                                    <option value="0">Tidak Aktif</option>
+                                </select>
+
+                                @error('status')
+                                    <span class="text-pink-500 text-sm">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+
+
+                            <button id="submit" type="submit"
+                                class="px-4 py-2  mt-8 w-full text-base font-semibold  bg-emerald-400 rounded-md border-none text-white btn-flash">
+                                Submit
+                            </button>
+
+                        </form>
                     </div>
                 </div>
             </div>
@@ -154,4 +191,43 @@
     <script src="/js/sidebar.js"></script>
     <script src="/js/dark-mode.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/datepicker.min.js"></script>
+
+    <script >
+        import {
+            Modal
+        } from 'flowbite';
+
+        window.addEventListener("load", (event) => {
+            if (!window.location.hash) {
+                return
+            }
+
+            let hash = window.location.hash.substring(1);
+            if (hash != "showNewSurveyModal") {
+                return
+            };
+
+            const $newSurveyModal = document.getElementById('newSurveyModal');
+            const options = {
+                placement: 'bottom-right',
+                backdrop: 'dynamic',
+                backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+                closable: true,
+                onHide: () => {
+                    console.log('modal is hidden');
+                },
+                onShow: () => {
+                    console.log('modal is shown');
+                },
+                onToggle: () => {
+                    console.log('modal has been toggled');
+                }
+            };
+
+            const modal = new Modal($targetEl, options);
+            modal.show();
+        });
+    </script>
+
+    {{--  --}}
 @endpush
