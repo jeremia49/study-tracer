bcef76349da01bfb25d52894e4b028baba446642
jeremia49 <39850621+jeremia49@users.noreply.github.com>
2023-06-01 10:15:48 +0700

    minor fix

--- resources/views/admin/survey.blade.php
+++ resources/views/admin/survey.blade.php
@@ -112,7 +112,7 @@
                                                 </a>
                                                 <div id="hp1" role="tooltip"
                                                     class="absolute z-10 invisible inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                                    delete
+                                                    Delete
                                                     <div class="tooltip-arrow" data-popper-arrow></div>
                                                 </div>
                                             </div>
@@ -168,7 +168,7 @@
                                                 </a>
                                                 <div id="hp1" role="tooltip"
                                                     class="absolute z-10 invisible inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                                    delete
+                                                    Delete
                                                     <div class="tooltip-arrow" data-popper-arrow></div>
                                                 </div>
                                             </div>
