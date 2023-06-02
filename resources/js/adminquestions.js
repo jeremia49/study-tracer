b1576e6db1d33883f43861eb75124d10fbe2048c
jeremia49 <39850621+jeremia49@users.noreply.github.com>
2023-06-02 13:31:29 +0700

    fix bug nomor on questionpage

--- resources/js/adminquestions.js
+++ resources/js/adminquestions.js
@@ -5,12 +5,12 @@ import JSON5 from 'json5'
 function createEl(angka, val=''){
     let wrapper= document.createElement('div');
     wrapper.innerHTML= `<div class="mb-6">
-        <label for="option${angka}"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Opsi ${angka}</label>
+        <label 
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white nomor-text">Opsi ${angka}</label>
         <div class="flex flex-row">
-            <input type="text" id="option${angka}"
+            <input type="text" 
                 class="flex-1 display-flex bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-borderForm focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-borderForm dark:focus:border-blue-500"
-                placeholder="Opsi ${angka}" required value="${val}" name="options[]">
+                required value="${val}" name="options[]">
             <button type="button"
                 class="ml-2 text-right text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 btn-delete-option"
                 >Hapus</button>
@@ -44,16 +44,18 @@ window.addEventListener("load", (event) => {
             if(curr.id == dataid) return true;
         }).options)
         
-        let i = 1
         dataoptions.forEach(function (item){
-            optionsContainer.append(createEl(i,item))
-            i+=1
+            optionsContainer.append(createEl(optionsContainer.children().length + 1,item))
         });
 
     })
 
     optionsContainer.on('click','.btn-delete-option',function(e){
         $(e.currentTarget.parentNode.parentNode).remove()
+
+        $('#optionContainer > div > label.nomor-text').each(function(i,obj){
+            obj.innerHTML = `Opsi ${i + 1}`;
+        })
     })
 
     $('#btnupdateOptions').on('click', function(){
